928f7124ac2cc0d019842dad1214b5ba06f5d2f1
Ronaldo Tacora <work.principal777@gmail.com>
2025-07-22 17:21:35 -0500

    leak_key

--- lib/backend/config/supabase_config.dart
+++ lib/backend/config/supabase_config.dart
@@ -1,4 +1,1 @@
-class SupabaseConfig {
-  static const String url = '';
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtY2Z2emR0b3RtdGpsdWZhc3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNDExMDQsImV4cCI6MjA2MDkxNzEwNH0.IoVRrR8-Mc1ZaOCrgCXKgWf0vm0rMufPfl1sfwYzrOA';
-}
+
