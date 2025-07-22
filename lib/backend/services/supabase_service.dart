6f7413d485eace54f03517185701d5d7fa4d1366
Ronaldo Tacora <work.principal777@gmail.com>
2025-07-22 17:22:26 -0500

    Update supabase_service.dart

--- lib/backend/services/supabase_service.dart
+++ lib/backend/services/supabase_service.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
-  static const String supabaseUrl = 'https://hmcfvzdtotmtjlufaszt.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtY2Z2emR0b3RtdGpsdWZhc3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNDExMDQsImV4cCI6MjA2MDkxNzEwNH0.IoVRrR8-Mc1ZaOCrgCXKgWf0vm0rMufPfl1sfwYzrOA';
+  static const String supabaseUrl = '...';
+  static const String supabaseAnonKey = '...';
 
   static Future<void> init() async {
     await Supabase.initialize(
